4c648bec0448fcfd9f4c1ce5b86b4bb1c927fb05
AkiraYumoto <juanicaehd@gmail.com>
2024-11-04 18:24:06 -0300

    Cambiamos el port en la variable de entorno

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
 const express = require('express');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log('Hello World!');
