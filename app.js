ff07afcb4074205e7fb9aaf3960a8af7b9462828
AkiraYumoto <juanicaehd@gmail.com>
2024-11-06 18:48:08 -0300

    Conexión con MongoDB + HTML CSS JS

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
-const dotenv = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
+
+mongoose
+    .connect(
+    'mongodb+srv://AkiraYumoto:Nuevachicago16@cluster0.j3vvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    )
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Servidor levantado en el puerto ${port}`)
+        });    
+        console.log('Conexión establecida')
+    })
+    .catch(err => console.log(err));
+
+const productSchema = mongoose.Schema(
+    {
+    name: {type : String, required: true},
+    price: Number
+    },{timestamps: true});
+
+const Product = mongoose.model('product', productSchema);
+
+app.use(express.json());
+
+app.post('/api/v1/products', (req, res,next) => {
+  const newProduct = new Product(req.body);
+
+  newProduct
+  .save()
+  .then((result) => res.status(201).json({ok: true}))
+  .catch((err) => console.log(err));
+   
+})
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    console.log('Hello World!');
-    res.status(200).send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Servidor levantado en el puerto ${port}`)
-});
